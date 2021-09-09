04e74fb63cdb7fcd977c31e3760a0579e7a4c3d6
Sarai <70646247+sarai-sansone@users.noreply.github.com>
2021-09-09 12:45:24 -0500

    Add files via upload

---
+++ virtualstoreweb/src/test/java/virtualstore/virtualstoreweb/VirtualstorewebApplicationTests.java
@@ -0,0 +1,13 @@
+package virtualstore.virtualstoreweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualstorewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
