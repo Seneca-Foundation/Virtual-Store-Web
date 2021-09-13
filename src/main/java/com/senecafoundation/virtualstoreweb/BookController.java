5343a6c950f89b173b05aa2c973546b59dc8dbd4
sarai-sansone <sarai.sansone@gmail.com>
2021-09-13 14:55:05 -0500

    book controller done

---
+++ src/main/java/com/senecafoundation/virtualstoreweb/BookController.java
@@ -0,0 +1,39 @@
+package com.senecafoundation.virtualstoreweb;
+
+import com.senecafoundation.virtualstoreweb.DataHandlers.RepoCreateData;
+import com.senecafoundation.virtualstoreweb.ProductObjects.BookObjects.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("comicbook")
+public class BookController {
+
+    @Autowired
+    RepoCreateData dataHandler;
+    
+    @GetMapping("/createform")
+    public String showForm(Model model) {
+        Book book = new Book();
+        model.addAttribute("book", book);
+        return "create_book";
+    }
+
+    @RequestMapping(value = "/createform", method = RequestMethod.POST)
+    public String submit(@ModelAttribute("comicbook") Book book, BindingResult result, ModelMap model) {
+        if (result.hasErrors()) {
+            return "error";
+        }
+        dataHandler.Create(book);
+        //repo.save(shadowElf);
+        model.addAttribute("book", book);
+        return "book";
+    }
+}
