200b03577b392293ca1089c36cff34f35ad59fde
Psanchez <87141029+Psanchez4501@users.noreply.github.com>
2021-09-15 10:23:23 -0500

    Matching basketball controller to templet

--- src/main/java/com/senecafoundation/virtualstoreweb/Controllers/BasketballController.java
+++ src/main/java/com/senecafoundation/virtualstoreweb/Controllers/BasketballController.java
@@ -13,27 +13,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("basketballtickets")
-public class BasketballticketController {
+@RequestMapping("basketball")
+public class BasketballController {
 
     @Autowired
     RepoCreateData dataHandler;
     
     @GetMapping("/createform")
     public String showForm(Model model) {
-        Basketball basketballTicket = new Basketball();
-        model.addAttribute("basketballticket", basketballTicket);
-        return "create_basketballticket";
+        Basketball basketball = new Basketball();
+        model.addAttribute("basketball", basketball);
+        return "create_basketball";
     }
 
     @RequestMapping(value = "/createform", method = RequestMethod.POST)
-    public String submit(@ModelAttribute("basketballticket") Basketball basketballTicket, BindingResult result, ModelMap model) {
+    public String submit(@ModelAttribute("basketball") Basketball basketball, BindingResult result, ModelMap model) {
         if (result.hasErrors()) {
             return "error";
         }
-        dataHandler.Create(basketballTicket);
+        dataHandler.Create(basketball);
         //repo.save(shadowElf);
-        model.addAttribute("basketballticket", basketballTicket);
-        return "basketballticket";
+        model.addAttribute("basketball", basketball);
+        return "basketball";
     }
 }
