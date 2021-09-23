5b581e349cf0f296f429ddf00aa8a7f839078b5b
sarai-sansone <sarai.sansone@gmail.com>
2021-09-23 11:15:05 -0500

    book delete endpoint created

--- src/main/java/com/senecafoundation/virtualstoreweb/Controllers/BookController.java
+++ src/main/java/com/senecafoundation/virtualstoreweb/Controllers/BookController.java
@@ -1,10 +1,13 @@
 package com.senecafoundation.virtualstoreweb.Controllers;
 
 import java.util.UUID;
+import java.util.List;
 
 import com.senecafoundation.virtualstoreweb.DataHandlers.RepoCreateData;
 import com.senecafoundation.virtualstoreweb.DataHandlers.RepoDeleteData;
+import com.senecafoundation.virtualstoreweb.DataHandlers.RepoReadData;
 import com.senecafoundation.virtualstoreweb.DataHandlers.RepoUpdateData;
+import com.senecafoundation.virtualstoreweb.FundamentalObjects.StoreItem;
 import com.senecafoundation.virtualstoreweb.ProductObjects.BookObjects.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -28,6 +31,8 @@ public class BookController {
     RepoUpdateData dataHandlerUpdate;
     @Autowired
     RepoDeleteData dataHandlerDelete;
+    @Autowired
+    RepoReadData dataHandlerRead;
     
     @GetMapping("/createform")
     public String showForm(Model model) {
@@ -54,6 +59,12 @@ public class BookController {
         }
         dataHandlerUpdate.Update(book);
         return "book";  
+    }
+    @GetMapping("/deleteform")
+    public String showDeleteForm(Model model) {
+        List<StoreItem> showItems = dataHandlerRead.ReadAll();
+        model.addAttribute("itemsToDelete", showItems);
+        return "delete_book";
     }
     @RequestMapping(value="/deleteform", method = RequestMethod.DELETE)
     public String erase(@ModelAttribute("book")Book book, UUID ID, BindingResult result, ModelMap model) {
