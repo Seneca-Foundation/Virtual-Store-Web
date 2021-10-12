9d94acec632cbb081025aa95e293877f7a2102f6
sarai-sansone <sarai.sansone@gmail.com>
2021-10-12 11:21:23 -0500

    book read all rest controller created & done

--- src/main/java/com/senecafoundation/virtualstoreweb/Controllers/BookRestController.java
+++ src/main/java/com/senecafoundation/virtualstoreweb/Controllers/BookRestController.java
@@ -1,15 +1,18 @@
 package com.senecafoundation.virtualstoreweb.Controllers;
 
+import java.util.List;
 import java.util.UUID;
 
 import com.senecafoundation.virtualstoreweb.DataHandlers.RepoDataHandlers.RepoCreateData;
 import com.senecafoundation.virtualstoreweb.DataHandlers.RepoDataHandlers.RepoDeleteData;
 import com.senecafoundation.virtualstoreweb.DataHandlers.RepoDataHandlers.RepoReadData;
 import com.senecafoundation.virtualstoreweb.DataHandlers.RepoDataHandlers.RepoUpdateData;
+import com.senecafoundation.virtualstoreweb.FundamentalObjects.StoreItem;
 import com.senecafoundation.virtualstoreweb.ProductObjects.BookObjects.Book;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,11 +30,17 @@ public class BookRestController {
     @Autowired
     RepoReadData<Book> dataHandlerRead;
 
+    @GetMapping("/books")
+    List<StoreItem> allBooks() {
+        return dataHandlerRead.ReadAll();
+    }
+
     @PostMapping("/books")
         Book newBook(@RequestBody Book newBook) {
         UUID newId = dataHandler.Create(newBook);
         return newBook;
     }
+
     @DeleteMapping("/books/{id}")
     void deleteBook(@PathVariable String id)
     {
