32347e7d16e03875ff26524e567ddaf4dc58d230
sarai-sansone <sarai.sansone@gmail.com>
2021-10-12 11:11:19 -0500

    book delete rest controller fixed & done

--- src/main/java/com/senecafoundation/virtualstoreweb/Controllers/BookRestController.java
+++ src/main/java/com/senecafoundation/virtualstoreweb/Controllers/BookRestController.java
@@ -9,6 +9,8 @@ import com.senecafoundation.virtualstoreweb.DataHandlers.RepoDataHandlers.RepoUp
 import com.senecafoundation.virtualstoreweb.ProductObjects.BookObjects.Book;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +31,11 @@ public class BookRestController {
         Book newBook(@RequestBody Book newBook) {
         UUID newId = dataHandler.Create(newBook);
         return newBook;
+    }
+    @DeleteMapping("/books/{id}")
+    void deleteBook(@PathVariable String id)
+    {
+        dataHandlerDelete.Delete(UUID.fromString(id));
     }
 }
 
