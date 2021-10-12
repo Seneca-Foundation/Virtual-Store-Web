56252eec6dc0fbf981428d5f5cc9a49b09917c11
sarai-sansone <sarai.sansone@gmail.com>
2021-10-12 11:21:57 -0500

    powerplant read all rest controller created & done

--- src/main/java/com/senecafoundation/virtualstoreweb/Controllers/PowerplantRestController.java
+++ src/main/java/com/senecafoundation/virtualstoreweb/Controllers/PowerplantRestController.java
@@ -1,15 +1,18 @@
 package com.senecafoundation.virtualstoreweb.Controllers;
 
+import java.util.List;
 import java.util.UUID;
 
 import com.senecafoundation.virtualstoreweb.DataHandlers.RepoDataHandlers.RepoCreateData;
 import com.senecafoundation.virtualstoreweb.DataHandlers.RepoDataHandlers.RepoDeleteData;
 import com.senecafoundation.virtualstoreweb.DataHandlers.RepoDataHandlers.RepoReadData;
 import com.senecafoundation.virtualstoreweb.DataHandlers.RepoDataHandlers.RepoUpdateData;
+import com.senecafoundation.virtualstoreweb.FundamentalObjects.StoreItem;
 import com.senecafoundation.virtualstoreweb.ProductObjects.Powerplant;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,6 +29,11 @@ class PowerplantRestController {
     RepoDeleteData<Powerplant> dataHandlerDelete;
     @Autowired
     RepoReadData<Powerplant> dataHandlerRead;
+
+    @GetMapping("/powerplants")
+    List<StoreItem> allPowerplants() {
+        return dataHandlerRead.ReadAll();
+    }
 
     @PostMapping("/powerplants")
         Powerplant newPowerplant(@RequestBody Powerplant newPowerplant) {
