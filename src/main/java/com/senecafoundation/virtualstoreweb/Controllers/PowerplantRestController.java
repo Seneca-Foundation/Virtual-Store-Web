f224084ebb7720b1439a283c5145c6b66784bf2a
maverick4200 <joshnava2@gmail.com>
2021-10-12 12:06:44 -0500

    Created powerplant REST update endpoint

--- src/main/java/com/senecafoundation/virtualstoreweb/Controllers/PowerplantRestController.java
+++ src/main/java/com/senecafoundation/virtualstoreweb/Controllers/PowerplantRestController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,6 +41,13 @@ class PowerplantRestController {
         UUID newId = dataHandler.Create(newPowerplant);
         return newPowerplant;
     }
+    @PutMapping("/powerplants/{id}")
+    Powerplant replacePowerplant(@RequestBody Powerplant newPowerplant, @PathVariable Long id) {
+
+        dataHandlerUpdate.Update(newPowerplant);
+        return newPowerplant;
+    }
+
     @DeleteMapping("/powerplants/{id}")
     void deletePowerplant(@PathVariable String id)
     {
