047a3053084d39934d1d22085e7123493735e46f
maverick4200 <joshnava2@gmail.com>
2021-10-12 14:06:19 -0500

    Basketball REST update created

--- src/main/java/com/senecafoundation/virtualstoreweb/Controllers/BasketballRestController.java
+++ src/main/java/com/senecafoundation/virtualstoreweb/Controllers/BasketballRestController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,6 +41,15 @@ class BasketballRestController {
         UUID newId = dataHandler.Create(newBasketball);
         return newBasketball;
     }
+    @PutMapping("/basketballs/{id}")
+    Basketball replaceBasketball(@RequestBody Basketball newBasketball, @PathVariable String id) {
+        Basketball basketball = (Basketball) dataHandlerRead.Read(UUID.fromString(id));
+        
+        dataHandlerUpdate.Update(newBasketball);
+        return newBasketball;
+    }
+
+
     @DeleteMapping("/basketballs/{id}")
     void deleteBasketball(@PathVariable String id)
     {
