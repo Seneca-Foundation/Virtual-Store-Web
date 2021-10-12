0d94f83e37147ec4f78711d3079854a3ece39e40
sarai-sansone <sarai.sansone@gmail.com>
2021-10-12 11:21:12 -0500

    basketball read all rest controller created & done

--- src/main/java/com/senecafoundation/virtualstoreweb/Controllers/BasketballRestController.java
+++ src/main/java/com/senecafoundation/virtualstoreweb/Controllers/BasketballRestController.java
@@ -1,15 +1,18 @@
 package com.senecafoundation.virtualstoreweb.Controllers;
 
+import java.util.List;
 import java.util.UUID;
 
 import com.senecafoundation.virtualstoreweb.DataHandlers.RepoDataHandlers.RepoCreateData;
 import com.senecafoundation.virtualstoreweb.DataHandlers.RepoDataHandlers.RepoDeleteData;
 import com.senecafoundation.virtualstoreweb.DataHandlers.RepoDataHandlers.RepoReadData;
 import com.senecafoundation.virtualstoreweb.DataHandlers.RepoDataHandlers.RepoUpdateData;
+import com.senecafoundation.virtualstoreweb.FundamentalObjects.StoreItem;
 import com.senecafoundation.virtualstoreweb.ProductObjects.Basketball;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,6 +29,11 @@ class BasketballRestController {
     RepoDeleteData<Basketball> dataHandlerDelete;
     @Autowired
     RepoReadData<Basketball> dataHandlerRead;
+
+    @GetMapping("/basketballs")
+    List<StoreItem> allBasketballs() {
+        return dataHandlerRead.ReadAll();
+    }
 
     @PostMapping("/basketballs")
     Basketball newBasketball(@RequestBody Basketball newBasketball) {
