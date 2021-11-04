fd915b909658d51c33de9f5c59f90aa57d22e5d6
sarai-sansone <sarai.sansone@gmail.com>
2021-11-04 11:10:19 -0500

    homepage image get in app controller, thymeleaf updated Co-authored-by: Warren Thompson <warrenjthompson@gmail.com> Co-authored-by: maverick4200 <maverick4200@users.noreply.github.com>

--- src/main/java/com/senecafoundation/virtualstoreweb/Controllers/AppController.java
+++ src/main/java/com/senecafoundation/virtualstoreweb/Controllers/AppController.java
@@ -1,5 +1,12 @@
 package com.senecafoundation.virtualstoreweb.Controllers;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
+import com.senecafoundation.virtualstoreweb.DataHandlers.RepoDataHandlers.RepoReadData;
+import com.senecafoundation.virtualstoreweb.ProductObjects.Bracelet;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,10 +15,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class AppController
 {
+  @Autowired
+  RepoReadData<Bracelet> dataHandlerReadBracelet;
+  // wire up the other three
 
-  @RequestMapping(method=RequestMethod.GET)
+  @RequestMapping(method=RequestMethod.GET, ModelMap model)
   public String index()
   {
+
+    model.addAttribute("braceletId", dataHandlerReadBracelet.ReadAll().get(0).getID().toString());
+    // do that for the other four categories
+
     return "index";
   }
 
