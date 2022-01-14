7269f0dc96da663a07a4d51dacc35e17ab463aa7
sarai-sansone <sarai.sansone@gmail.com>
2022-01-13 21:50:40 -0600

    user class getters & setters for fields

--- src/main/java/com/senecafoundation/virtualstoreweb/User.java
+++ src/main/java/com/senecafoundation/virtualstoreweb/User.java
@@ -23,6 +23,44 @@ public class User {
      
     @Column(name = "last_name", nullable = false, length = 20)
     private String lastName;
-     
-    // getters and setters are not shown   
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
